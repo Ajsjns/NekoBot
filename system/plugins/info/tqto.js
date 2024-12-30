9de7a21c5692661803d9eac10dbdb99cec0212f9
aayoubayoub2781 <148594510+aayoubayoub2781@users.noreply.github.com>
2024-12-30 14:40:11 +0100

    Update tqto.js

--- system/plugins/info/tqto.js
+++ system/plugins/info/tqto.js
@@ -1,29 +1,9 @@
 module.exports = {
-  command: "tqto",
+  command: "channel",
   alias: ["credit"],
   category: ["info"],
-  description: "List Contrubutor bot ini",
+  description: "قناة صانع البوت",
   async run(m) {
-    let cap = `*>_ TERIMAKASIH KEPADA :*
+    let cap = `*>_اشترك من فضلك_* :*
         
-> *-* Bang_syaii ( pembuat sc & pembuat scrape bot )
-> *- Telegram :* https://t.me/this_syaii
-
-> *-* AxellNetwork ( Pengembang sc & pembuat scrape bot )
-> *- Github :* https://github.com/AxellNetwork
-
-> *-* Pengguna Script ( Kalian semua )
-
-^-^ Terimakasih sudah menggunakan script kami
-semoga script kami bermanfaat untuk anda yang menggunakan/tidak menggunakan nya
-
-– Support terus project kami lainnya di :
-https://github.com/AxellNetwork
-
-*– Forum Update :*
-https://whatsapp.com/channel/0029VauJgduEwEjwwVwLnw37
-
-https://chat.whatsapp.com/BsZHPiZoisT5GdVgiEufJK`;
-    m.reply(cap);
-  },
-};
+https://whatsapp.com/channel/0029VayqYQDBVJl5mVZsit1G
