72f87700d71628fe739454aa13efb5becb557c93
aayoubayoub2781 <148594510+aayoubayoub2781@users.noreply.github.com>
2025-01-01 00:26:41 +0100

    Add files via upload

--- system/plugins/events/antilink.js
+++ system/plugins/events/antilink.js
@@ -4,16 +4,16 @@ async function events(m, { sock, Func }) {
   if (!m.isGroup) return
   let group = db.list().group[m.cht]
   if (Func.isUrl(m.body) && /chat.whatsapp.com/.test(m.body)) {
-  if (!m.isBotAdmin) return
-   let link = await getUrlInfo(Func.isUrl(m.body).find((a) => a.includes("chat.whatsapp.com")));
-   let msg = `*– 乂 Link Group Terdeteksi !*\n`
-       msg += `> *- Tag :* @${m.sender.split("@")[0]}\n`
-       msg += `> *- Status :* ${m.isAdmin ? "admin group" : "member group"}`
-       msg += `\n\n${m.isAdmin ? `> Kamu aman karena kamu admin dari group ${m.metadata.subject}` :   `> Maaf Kami tidak memperbolehkan anda mengirim *${link.title}* cari group lain saja 😹`}`
-   if (!m.isAdmin) return m.reply(msg)
-    .then(() => {
-     m.reply({ delete: m.key });
-   })
+    if (!m.isBotAdmin) return
+    let link = await getUrlInfo(Func.isUrl(m.body).find((a) => a.includes("chat.whatsapp.com")));
+    let msg = `*– 乂 تم اكتشاف رابط مجموعة!*\n`
+        msg += `> *- الوسم :* @${m.sender.split("@")[0]}\n`
+        msg += `> *- الحالة :* ${m.isAdmin ? "مشرف في المجموعة" : "عضو في المجموعة"}`
+        msg += `\n\n${m.isAdmin ? `> أنت آمن لأنك مشرف في المجموعة ${m.metadata.subject}` :   `> عذرًا، لا يُسمح لك بإرسال رابط *${link.title}* ابحث عن مجموعة أخرى فقط 😹`}`
+    if (!m.isAdmin) return m.reply(msg)
+      .then(() => {
+        m.reply({ delete: m.key });
+      })
   }
 }
 
